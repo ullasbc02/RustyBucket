27885fe5f1531c66caf32a76a6718b33939709eb
ullasbc <ullasbc02@gmail.com>
2026-04-22 22:36:17 -0400

    basics: move hello_rust to basics

--- basics/hello_rust/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("Hello, world!");
-
-    let x = 5;
-    println!("The value of x is: {}", x);
-}
---
+++ basics/src/main.rs
@@ -0,0 +1,72 @@
+// fn add(a: i32, b: i32) -> i32 {
+//     a + b
+// }
+fn main() {
+
+    // declaring of mutable variable and shadowing of variable
+    // let x = 5;
+    // let x = 3;
+    // println!("This is the value of x: {}", x);
+
+    // declaring of constant variable
+    // const PI: f64 = 3.14159;
+    // println!("The value of PI is: {}", PI);
+
+    // declaring datatypes
+    // let x: i32 = 5;
+    // let y: f64 = 3.14;
+    // let z: bool = true;
+    // let name: &str = "Ullas";
+    // println!("Integer: {}, Float: {}, Boolean: {}, String: {}", x, y, z, name);
+
+    // shadowing of variable
+    // let x = 5;
+    // let x = x + 1; // shadowing the previous value of x
+    // let x = x * 2; // shadowing again
+    // println!("The value of x is: {}", x);
+
+    // calling a function
+    // let result = add(5, 3);
+    // println!("The result is: {}", result);
+
+    // using a loop
+    // for i in 0..5 {
+    //     println!("The value of i is: {}", i);
+    // }
+
+    // using an if statement
+    // let number = 10;
+    // if number < 5 {
+    //     println!("The number is less than 5");
+    // } else if number == 5 {
+    //     println!("The number is equal to 5");
+    // } else {
+    //     println!("The number is greater than 5");
+    // }
+
+    // using a match statement similar to switch case in other languages
+    // let number = 2;
+    // match number {
+    //     1 => println!("One"),
+    //     2 => println!("Two"),
+    //     3 => println!("Three"),
+    //     _ => println!("Other"),
+    // }
+
+    // using a vector
+    // let mut numbers = vec![1, 2, 3, 4, 5];
+    // numbers.push(6);
+    // println!("The numbers are: {:?}", numbers);
+    // for number in &numbers {
+    //     println!("The number is: {}", number);
+    // }
+
+
+    // using a tuple
+    // let person: (&str, i32) = ("Ullas", 30);
+    // println!("Name: {}, Age: {}", person.0, person.1);
+
+
+
+}
+
